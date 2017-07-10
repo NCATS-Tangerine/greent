8ad86965c914ee0f9209f142d1ffe523541f692b
Steven Cox <scox@renci.org>
2017-07-10 09:53:38 -0400

    graphql app

--- greent/core.py
+++ greent/core.py
@@ -1,6 +1,5 @@
 import logging
 import json
-import graphene
 import pprint
 import unittest
 from exposures import Exposures
@@ -17,53 +16,13 @@ class LoggingUtil(object):
         FORMAT = '%(asctime)-15s %(filename)s %(funcName)s %(levelname)s: %(message)s'
         logging.basicConfig(format=FORMAT, level=logging.INFO)
         return logging.getLogger(name)
+
 logger = LoggingUtil.init_logging (__file__)
 
-''' Use GraphQL to query the RENCI Environmental Exposures API. '''
-
-class ExposureScore(graphene.ObjectType):
-    ''' An exposure score. '''
-    exposure_type = graphene.String ()
-    start_time    = graphene.String ()
-    end_time      = graphene.String ()
-    latitude      = graphene.Float ()
-    longitude     = graphene.Float ()
-    units         = graphene.String ()
-    value         = graphene.String ()
-
-class ExposureValue(graphene.ObjectType):
-    ''' An exposure value. '''
-    exposure_type = graphene.String ()
-    start_time    = graphene.String ()
-    end_time      = graphene.String ()
-    latitude      = graphene.Float ()
-    longitude     = graphene.Float ()
-    units         = graphene.String ()
-    value         = graphene.String ()
-
-class QueryExposureScores (graphene.ObjectType):
-    exposure_scores = graphene.List (of_type=ExposureScore,
-                                     description="Exposure scores")
-    def resolve_exposure_scores (self, args, context, info):
-        exposures = Exposures ()
-        return exposures.get_scores (
-            exposure_type  = args.get ("exposureType"),
-            start_date     = args.get ("startDate"),
-            end_date       = args.get ("endDate"),
-            exposure_point = args.get ("exposurePoint"))
-
-class QueryExposureValues (graphene.ObjectType):
-    exposure_values = graphene.List (of_type=ExposureValue,
-                                     description="Exposure values")
-    def resolve_exposure_values (self, args, context, info):
-        exposures = Exposures ()
-        return exposures.get_values (
-            exposure_type  = args.get ("exposureType"),
-            start_date     = args.get ("startDate"),
-            end_date       = args.get ("endDate"),
-            exposure_point = args.get ("exposurePoint"))
-
 class GreenT (object):
+
+    ''' The Green Translator API - a single Python interface aggregating access mechanisms for 
+    all Green Translator services. '''
 
     def __init__(self, config={}):
         self.config = config
@@ -74,52 +33,28 @@ class GreenT (object):
         if not blaze_uri:
             blaze_uri = 'http://stars-blazegraph.renci.org/bigdata/sparql'
         self.blazegraph = TripleStore (blaze_uri)
+
         self.chembio_ks = ChemBioKS (self.blazegraph)
         self.clinical = Clinical ()
         self.exposures = Exposures ()
 
-    def get_exposure_query (self, query_tag, exposure_type, start_date, end_date, exposure_point):
-        query = Template ("""{
-           ${query_tag} {
-              value,
-              units,
-              latitude,
-              longitude,
-              exposureType,
-              startTime,
-              endTime
-           }
-        }""").substitute (query_tag=query_tag)
+    # Exposure API
 
-        context = {
-            "exposureType"  : exposure_type,
-            "startDate"     : start_date,
-            "endDate"       : end_date,
-            "exposurePoint" : exposure_point
-        }
-        return query, context
-    
     def get_exposure_scores (self, exposure_type, start_date, end_date, exposure_point):
-        query, context = self.get_exposure_query ("exposureScores", exposure_type, start_date, end_date, exposure_point)
-        return graphene.Schema (query=QueryExposureScores).execute (query, context)
+        return self.exposures.get_scores (
+            exposure_type = exposure_type,
+            start_date = start_date,
+            end_date = end_date,
+            exposure_point = exposure_point)
 
     def get_exposure_values (self, exposure_type, start_date, end_date, exposure_point):
-        query, context = self.get_exposure_query ("exposureValues", exposure_type, start_date, end_date, exposure_point)
-        return graphene.Schema (query=QueryExposureValues).execute (query, context)
+        return self.exposures.get_values (
+            exposure_type  = exposure_type,
+            start_date     = start_date,
+            end_date       = end_date,
+            exposure_point = exposure_point)
 
-    def print_exposure_results (self, result, key):
-        if result.errors:
-            print ("errors: {0}".format (result.errors))
-        else:
-            for result in result.data[key]:
-                print( "  --score: type: {0} lat: {1} lon: {2} start: {3} end: {4} unit: {5} val: {6}".format (
-                    result['exposureType'],
-                    result['latitude'],
-                    result['longitude'],
-                    result['startTime'],
-                    result['endTime'],
-                    result['units'],
-                    result['value']))
+    # ChemBio API
 
     def get_exposure_conditions (self, chemicals):
         return self.chembio_ks.get_exposure_conditions (chemicals)
@@ -130,9 +65,7 @@ class GreenT (object):
     def get_genes_pathways_by_disease (self, diseases):
         return self.chembio_ks.get_genes_pathways_by_disease (diseases)
 
-
-    # Clinical:
+    # Clinical API
 
     def get_patients (self, age=None, sex=None, race=None, location=None):
         return self.clinical.get_patients (age, sex, race, location)
-
--- greent/schema.py
+++ greent/schema.py
@@ -10,24 +10,34 @@ def resolve_raises(*_):
 greenT = GreenT ()
 
 def get_exposure_scores (obj, args, context, info):
-    return greenT.exposures.get_scores (
+    return greenT.get_exposure_scores (
         exposure_type  = args.get ("exposureType"),
         start_date     = args.get ("startDate"),
         end_date       = args.get ("endDate"),
         exposure_point = args.get ("exposurePoint"))
     
 def get_exposure_values (obj, args, context, info):
-    return greenT.exposures.get_values (
+    return greenT.get_exposure_values (
         exposure_type  = args.get ("exposureType"),
         start_date     = args.get ("startDate"),
         end_date       = args.get ("endDate"),
         exposure_point = args.get ("exposurePoint"))
 
 def get_patients (obj, args, context, info):
-    return greenT.get_patients (age = args.get ("age"),
-                                sex = args.get ("sex"),
-                                race = args.get ("race"),
-                                location = args.get ("location"))
+    return greenT.get_patients (
+        age = args.get ("age"),
+        sex = args.get ("sex"),
+        race = args.get ("race"),
+        location = args.get ("location"))
+
+def get_exposure_conditions (obj, args, context, info):
+    return greenT.get_exposure_conditions (chemicals = args.get ("chemicals"))
+
+def get_drugs_by_condition (obj, args, context, info):
+    return greenT.get_drugs_by_condition (conditions = args.get ("conditions"))
+
+def get_genes_pathways_by_disease (obj, args, context, info):
+    return greenT.get_genes_pathways_by_condition (conditions = args.get ("diseases"))
 
 QueryRootType = GraphQLObjectType(
     name='QueryRoot',
@@ -59,18 +69,48 @@ QueryRootType = GraphQLObjectType(
         'exposureScore' : GraphQLField (
             type=GraphQLString,
             args = {
-                'type' : GraphQLArgument (GraphQLString)
+                'type' : GraphQLArgument (GraphQLString),
+                'startDate' : GraphQLArgument (GraphQLString),
+                'endDate' : GraphQLArgument (GraphQLString),
+                'exposurePoint' : GraphQLArgument (GraphQLString)
             },
             resolver = get_exposure_scores
         ),
+
         'exposureValue' : GraphQLField (
             type=GraphQLString,
             args = {
-                'type' : GraphQLArgument (GraphQLString)
-            },
+                'type' : GraphQLArgument (GraphQLString),
+                'startDate' : GraphQLArgument (GraphQLString),
+                'endDate' : GraphQLArgument (GraphQLString),
+                'exposurePoint' : GraphQLArgument (GraphQLString)
+           },
             resolver = get_exposure_values
-        )
+        ),
 
+        'exposureConditions' : GraphQLField (
+            type = GraphQLString,
+            args = {
+                'chemicals' : GraphQLArgument (GraphQLString) 
+            },
+            resolver = get_exposure_conditions            
+        ),
+
+        'drugsByCondition' : GraphQLField (
+            type = GraphQLString,
+            args = {
+                'conditions' : GraphQLArgument (GraphQLString)                 
+            },
+            resolver = get_drugs_by_condition
+        ),
+
+        'genesPathwaysByDisease' : GraphQLField (
+            type = GraphQLString,
+            args = {
+                'diseases' : GraphQLArgument (GraphQLString)                 
+            },
+            resolver = get_genes_pathways_by_disease
+        )
 
     }
 )
