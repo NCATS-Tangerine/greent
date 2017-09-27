71299878378c9cfd7ee6813e2b70ff1dda50449e
Steven Cox <scox@renci.org>
2017-09-26 20:15:43 -0400

    new spec.

--- greent/cmaq.py
+++ greent/cmaq.py
@@ -3,24 +3,27 @@ import json
 import requests
 import traceback
 import unittest
-from datetime import date
-from datetime import datetime, timedelta
+import datetime
+#from datetime import date
+#from datetime import datetime, timedelta
 from bravado.client import SwaggerClient
 from bravado.requests_client import RequestsClient
 from string import Template
+from pprint import pprint
 
 class SwaggerEndpoint(object):
 
     def __init__(self, swagger_endpoint_url):
         http_client = RequestsClient ()
         # TODO: fix ssl certificate.
-        http_client.session.verify = False #"/Users/scox/dev/venv/trans/lib/python3.6/site-packages/certifi/weak.pem" #False
+        #http_client.session.verify = False #"/Users/scox/dev/venv/trans/lib/python3.6/site-packages/certifi/weak.pem" #False
         self.client = SwaggerClient.from_url(
             swagger_endpoint_url,
             http_client=http_client,
             config={
             #    "validate_requests" : False,
-                "validate_responses" : False
+            #    "validate_responses" : False
+                'use_models': False
             })
 
     def inspect (self):
@@ -38,46 +41,43 @@ class CMAQ (SwaggerEndpoint):
 
     
     def get_meta (self):
-        return self.client.default.controllers_default_controller_cmaq_get ().result ()
+#        return self.client.default.controllers_default_controller_cmaq_get ().result ()
+        return self.client.cmaq.get_cmaq ().result (timeout = 10)
     
     def get_scores (self, start_date, end_date, lat_lon, exposureType='pm25', resolution='7day', aggregation='max', utcOffset='utc'):
-        return self.client.default.controllers_default_controller_cmaq_get_scores_get (
+#        return self.client.default.controllers_default_controller_cmaq_get_scores_get (
+        return self.client.cmaq.get_cmaq_getScores (
             exposureType = exposureType,
-            startDate = start_date,
-            endDate = end_date, 
+            startDate = datetime.datetime.strptime(start_date, "%Y-%m-%d").date (),
+            endDate = datetime.datetime.strptime(end_date, "%Y-%m-%d").date (),
 	    latLon = lat_lon,
             resolution = resolution,
             aggregation = aggregation,
-            utcOffset = utcOffset).result ()
+            utcOffset = utcOffset).result (timeout = 10)
 
     def get_values (self, start_date, end_date, lat_lon, exposureType='pm25', resolution='7day', aggregation='max', utcOffset='utc'):
-        return self.client.default.controllers_default_controller_cmaq_get_values_get (
+#        return self.client.default.controllers_default_controller_cmaq_get_values_get (
+        return self.client.cmaq.get_cmaq_getValues (
             exposureType = exposureType,
-            startDate = start_date,
-            endDate = end_date, 
+            startDate = datetime.datetime.strptime(start_date, "%Y-%m-%d").date (),
+            endDate = datetime.datetime.strptime(end_date, "%Y-%m-%d").date (),
 	    latLon = lat_lon,
             resolution = resolution,
             aggregation = aggregation,
             utcOffset = utcOffset).result ()
 
-c = CMAQ ("https://exposures.renci.org/v1/swagger.json")
-
+#c = CMAQ ("https://exposures.renci.org/v1/swagger.json")
+#c = CMAQ ("https://raw.githubusercontent.com/RENCI/nih-exposures-api/master/specification/swagger.yml")
+c = CMAQ ("https://app.swaggerhub.com/apiproxy/schema/file/mjstealey/environmental_exposures_api/0.0.1/swagger.json")
 c.inspect ()
 
-print (json.dumps (c.get_meta (), indent=2))
+pprint (c.get_scores (start_date = "2011-01-01",
+                      end_date = "2011-12-31",
+                      lat_lon = "35.9131996,-79.0558445"))
 
-#print (json.dumps (
-if True:
-    
-    print (json.dumps (c.get_scores (start_date = date (2010, 1, 1),
-                                    end_date = date (2010, 12, 31),
-                                    lat_lon = "35.9131996,-79.0558445"),
-                       indent=2))
-
-    print (json.dumps (c.get_values (start_date = date (2011, 1, 1),
-                                     end_date = date (2011, 12, 31),
-                                     lat_lon = "35.9131996,-79.0558445"),
-                       indent=2))
+pprint (c.get_values (start_date = "2011-01-01",
+                      end_date = "2011-12-31",
+                      lat_lon = "35.9131996,-79.0558445"))
 
 '''
 {
@@ -88,7 +88,7 @@ if True:
           "max",
           "avg"
         ]
-      ],
+       ],
       "endDate": "2011-12-31",
       "exposureType": "pm25",
       "exposureUnit": "ugm3",
