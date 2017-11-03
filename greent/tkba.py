7061391a14a17dbdc2d5e04a825ef9d40eb7ed1d
cbizon <bizon@renci.org>
2017-11-03 09:07:37 -0400

    Wrapped concept argument in double quotes to avoid spurious tokenizing

--- greent/tkba.py
+++ greent/tkba.py
@@ -15,7 +15,8 @@ class TranslatorKnowledgeBeaconAggregator(Service):
         super(TranslatorKnowledgeBeaconAggregator, self).__init__("tkba", context)
 
     def request_concept (self, concept):
-        url = "{0}/concepts?keywords={1}".format (self.url, Text.un_curie (concept.identifier))
+        #Without quotes around the keyword, this function treats space as a delimiter...
+        url = '{0}/concepts?keywords="{1}"'.format (self.url, Text.un_curie (concept.identifier))
         return requests.get (url).json ()
 
     def name_to_doid (self, name):
