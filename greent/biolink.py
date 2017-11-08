cbfd61a412ede1f76a5c731327181f2cb451958d
cbizon <bizon@renci.org>
2017-11-08 11:32:02 -0500

    Got as much as we can from edges for the moment

--- greent/biolink.py
+++ greent/biolink.py
@@ -16,17 +16,20 @@ class Biolink(Service):
         super(Biolink, self).__init__("biolink", context)
         self.checker = Mondo(ServiceContext.create_context ())
         self.go = GO(ServiceContext.create_context ())
-    def process_associations(self, r, target_node_type):
+    def process_associations(self, r, predicate, target_node_type):
         edge_nodes = [ ]
         for association in r['associations']:
+            pubs = []
             if 'publications' in association and association['publications'] is not None:
-                pubs = [ {'id': pub['id']} for pub in association['publications'] ]
-            else:
-                pubs = []
+                for pub in association['publications']:
+                    #Sometimes, we get back something like "uniprotkb" instead of a PMID.  We don't want it.
+                    pubid_prefix = pub['id'][:4].upper()
+                    if pubid_prefix == 'PMID':
+                        pubs.append( pub['id'] )
             obj = KNode(association['object']['id'], target_node_type, association['object']['label'] )
             rel = { 'typeid': association['relation']['id'], 'label':association['relation']['label'] }
             props = { 'publications': pubs, 'relation':rel }
-            edge = KEdge( 'biolink', 'gene_get_disease', props )
+            edge = KEdge( 'biolink', predicate , props )
             edge_nodes.append( (edge , obj ) )
         return edge_nodes
     def gene_get_disease(self, gene_node):
@@ -34,30 +37,31 @@ class Biolink(Service):
         ehgnc = urllib.parse.quote_plus(gene_node.identifier)
         logging.getLogger('application').debug('          biolink: %s/bioentity/gene/%s/diseases' % (self.url, ehgnc))
         r = requests.get('%s/bioentity/gene/%s/diseases' % (self.url, ehgnc)).json()
-        return self.process_associations( r, node_types.DISEASE )
+        return self.process_associations( r, 'gene_get_disease', node_types.DISEASE )
     def disease_get_phenotype(self,disease):
         url = "{0}/bioentity/disease/{1}/phenotypes/".format (self.url, disease.identifier )
         response = requests.get (url).json ()
-        return self.process_associations( response, node_types.PHENOTYPE )
+        return self.process_associations( response, 'disease_get_phenotype', node_types.PHENOTYPE )
     def gene_get_go(self,gene):
         #this function is very finicky.  gene must be in uniprotkb, and the curie prefix must be correctly capitalized
         url = "{0}/bioentity/gene/UniProtKB:{1}/function/".format (self.url, Text.un_curie(gene.identifier) )
         response = requests.get (url).json ()
         #return [ (a['object']['id'] , a['object']['label']) for a in response['associations'] ]
-        return self.process_associations(response, node_types.PROCESS)
+        return self.process_associations(response, 'gene_get_go', node_types.PROCESS)
     def gene_get_function (self, gene):
         edges_nodes = self.gene_get_go( gene )
         process_results = list( filter( lambda x: self.go.is_molecular_function(x[1].identifier), edges_nodes ) )
         for edge, node in process_results:
-            edge.predicate='molecular_function'
+            edge.predicate='gene_get_molecular_function'
             node.identifier.replace('GO:', 'GO.MOLECULAR_FUNCTION:')
             node.node_type = node_types.FUNCTION
         return process_results
     def gene_get_process ( self, gene):
+        print (gene.identifier)
         edges_nodes = self.gene_get_go( gene )
         process_results = list( filter( lambda x: self.go.is_biological_process(x[1].identifier), edges_nodes ) )
         for edge, node in process_results:
-            edge.predicate='biological_process'
+            edge.predicate='gene_get_biological_process'
             node.identifier.replace('GO:', 'GO.BIOLOGICAL_PROCESS:')
             node.node_type = node_types.PROCESS
         return process_results
