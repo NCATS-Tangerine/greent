16a14ea48d30e8ce9bec6d84bd249517af183e0d
patrickkwang <patrick@covar.com>
2018-05-10 14:57:10 -0400

    spelling fix

--- builder/omnicorp.py
+++ builder/omnicorp.py
@@ -23,14 +23,14 @@ class OmnicorpSupport():
         logger.debug(f'OmniCorp {node_a.identifier} {node_b.identifier} -> {len(articles)}')
         #Even if articles = [], we want to make an edge for the cache. We can decide later to write it or not.
         pmids = [f'PMID:{x.split("/")[-1]}' for x in articles]
-        predicate=LabeledID('omnicorp:1', 'literature_co-ocurrence')
+        predicate=LabeledID('omnicorp:1', 'literature_co-occurrence')
         ke = KEdge(node_a, node_b, 'omnicorp.term_to_term', dt.now(), predicate,predicate,
                    f'{node_a.identifier},{node_b.identifier}',publications=pmids, is_support=True)
         return ke
 
     def generate_all_edges(self, nodelist):
         results = self.omnicorp.get_all_shared_pmids(nodelist)
-        predicate=LabeledID('omnicorp:1', 'literature_co-ocurrence')
+        predicate=LabeledID('omnicorp:1', 'literature_co-occurrence')
         edges = [ KEdge(k[0], k[1], 'omnicorp.term_to_term', dt.now(), predicate,predicate,
                         f'{k[0].identifier},{k[1].identifier}', publications=v, is_support=True)
                   for k,v in results.items()]
