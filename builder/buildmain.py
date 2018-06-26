ab656762f680f6e525ab4c4814f1cc0ab735a9bb
patrickkwang <patrick@covar.com>
2018-06-26 13:00:01 -0400

    try/except is more "pythonic"

--- builder/buildmain.py
+++ builder/buildmain.py
@@ -90,15 +90,16 @@ class KnowledgeGraph:
 
     def find_node(self, node):
         """If node exists in graph, return it, otherwise, return None"""
-        if node.identifier in self.node_map:
-            return self.node_map[node.identifier]
         #We need to be less promiscuous here.   One thing that can happen is that OMIMs can unify what we consider
         # diseases and what we consider genes.  For now, we'll assume that our synonymization/normalization is working
         # well and # we don't have to sweat this.
         #for syn in node.synonyms:
         #    if syn in self.node_map:
         #        return self.node_map[syn]
-        return None
+        try:
+            return self.node_map[node.identifier]
+        except KeyError:
+            return None
 
     def add_or_find_node(self, node):
         """Find a node that already exists in the graph, checking for synonyms.
