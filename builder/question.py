a91fa2d074c8ee1d80ce859c6c568c508bc7f492
patrickkwang <patrick@covar.com>
2018-06-26 13:01:26 -0400

    build cypher query-generation functionality into new question class

--- builder/question.py
+++ builder/question.py
@@ -41,8 +41,21 @@ class Node(FromDictMixin):
 
         super().__init__(*args, **kwargs)
 
-    def dump(self):
-        return {**vars(self)}
+    def concept_cypher_signature(self, id, known=False):
+        if self.type and not known:
+            return f"({id}:Concept {{name: '{self.type}'}})"
+        elif not known:
+            return f"({id}:Concept)"
+        else:
+            return f"({id})"
+
+    def cypher_signature(self, id, known=False):
+        if self.curie and self.type and not known:
+            return f"({id}:{self.type} {{id: '{self.curie}'}})"
+        elif self.type and not known:
+            return f"({id}:{self.type})"
+        else:
+            return f"({id})"
 
 @swagger.definition('Edge')
 class Edge(FromDictMixin):
@@ -52,12 +65,12 @@ class Edge(FromDictMixin):
     schema:
         id: Edge
         required:
-            - start
-            - end
+            - source_id
+            - target_id
         properties:
-            start:
+            source_id:
                 type: string
-            end:
+            target_id:
                 type: string
             min_length:
                 type: integer
@@ -67,15 +80,32 @@ class Edge(FromDictMixin):
                 default: 1
     """
     def __init__(self, *args, **kwargs):
-        self.start = None
-        self.end = None
+        self.source_id = None
+        self.target_id = None
         self.min_length = 1
         self.max_length = 1
 
         super().__init__(*args, **kwargs)
 
-    def dump(self):
-        return {**vars(self)}
+        if self.min_length > self.max_length:
+            raise ValueError("An edge's minimum length should be less than or equal to its maximum length.")
+
+    def concept_cypher_signature(self):
+        if not self.min_length == self.max_length == 1:
+            return f"-[*{self.min_length}..{self.max_length}]-"
+        else:
+            return  "--"
+
+    def cypher_signature(self, id=None):
+        # TODO: handle known predicates
+        if id and not self.min_length == self.max_length == 1:
+            return f"-[{id}*{self.min_length}..{self.max_length}]-"
+        elif id:
+            return f"-[{id}]-"
+        elif not self.min_length == self.max_length == 1:
+            return f"-[*{self.min_length}..{self.max_length}]-"
+        else:
+            return  "--"
 
 @swagger.definition('Question')
 class Question(FromDictMixin):
@@ -85,6 +115,11 @@ class Question(FromDictMixin):
     schema:
         id: Question
         required:
+          - machine_question
+        properties:
+            machine_question:
+                type: object
+                required:
           - nodes
           - edges
         properties:
@@ -99,24 +134,69 @@ class Question(FromDictMixin):
     """
 
     def __init__(self, *args, **kwargs):
-        '''
-        keyword arguments: id, user, notes, natural_question, nodes, edges
-        q = Question(kw0=value, ...)
-        q = Question(struct, ...)
-        '''
-        # initialize all properties
         self.machine_question = {}
 
         super().__init__(*args, **kwargs)
 
-    def preprocess(self, key, value):
+    def load_attribute(self, key, value):
         if key == 'machine_question':
             return {
                 'nodes': [Node(n) for n in value['nodes']],
                 'edges': [Edge(e) for e in value['edges']]
             }
         else:
-            return None
+            return super().load_attribute(key, value)
 
-    def dump(self):
-        return {**vars(self)}
+    @property
+    def cypher_signature(self):
+        node_map = {n.id:n for n in self.machine_question['nodes']}
+        links = []
+        known_ids = set()
+        for e in self.machine_question['edges']:
+            source_signature = node_map[e.source_id].cypher_signature(
+                f'n{e.source_id}',
+                known=e.source_id in known_ids
+            )
+            target_signature = node_map[e.target_id].cypher_signature(
+                f'n{e.target_id}',
+                known=e.target_id in known_ids
+            )
+            links.append(f"{source_signature}{e.cypher_signature()}{target_signature}")
+            known_ids.update([e.source_id, e.target_id])
+        return links
+
+    @property
+    def concept_cypher_signature(self):
+        node_map = {n.id:n for n in self.machine_question['nodes']}
+        links = []
+        known_ids = set()
+        for e in self.machine_question['edges']:
+            source_signature = node_map[e.source_id].concept_cypher_signature(
+                f'n{e.source_id}',
+                known=e.source_id in known_ids
+            )
+            target_signature = node_map[e.target_id].concept_cypher_signature(
+                f'n{e.target_id}',
+                known=e.target_id in known_ids
+            )
+            links.append(f"{source_signature}{e.cypher_signature()}{target_signature}")
+            known_ids.update([e.source_id, e.target_id])
+        return links
+
+    @property
+    def named_nodes(self):
+        return [n.id for n in self.machine_question['nodes'] if n.curie]
+
+    def generate_concept_cypher(self):
+        """Generate a cypher query to find paths through the concept-level map."""
+        links = self.concept_cypher_signature
+        num_links = len(links)
+        path_names = [f"p{idx}" for idx in range(num_links)]
+        cypherbuffer = [f"MATCH {p}={s}" for p, s in zip(path_names, self.concept_cypher_signature)]
+        nodes = ' + '.join([f'nodes({p})' for p in path_names])
+        relationships = ' + '.join([f'relationships({p})' for p in path_names])
+        cypherbuffer.append(f'WHERE robokop.traversable({nodes}, {relationships}, [{", ".join([f"n{idx}" for idx in self.named_nodes])}])')
+        # This is to make sure that we don't get caught up in is_a and other funky relations.:
+        cypherbuffer.append(f'AND ALL(r in {relationships} WHERE EXISTS(r.op))')
+        cypherbuffer.append(f"RETURN {', '.join(path_names)}")
+        return '\n'.join(cypherbuffer)
--- builder/util.py
+++ builder/util.py
@@ -1,5 +1,4 @@
 import warnings
-import networkx as nx
 
 class FromDictMixin():
     def __init__(self, *args, **kwargs):
@@ -11,16 +10,32 @@ class FromDictMixin():
             struct = args[0]
             for key in struct:
                 if key in attributes:
-                    setattr(self, key, self.preprocess(key, struct[key]))
+                    setattr(self, key, self.load_attribute(key, struct[key]))
                 else:
                     warnings.warn("JSON field {} ignored.".format(key))
 
         # override any json properties with the named ones
         for key in kwargs:
             if key in attributes:
-                setattr(self, key, self.preprocess(key, kwargs[key]))
+                setattr(self, key, self.load_attribute(key, kwargs[key]))
             else:
                 warnings.warn("Keyword argument {} ignored.".format(key))
 
-    def preprocess(self, key, value):
+    def load_attribute(self, key, value):
         return value
+
+    def dump(self):
+        prop_dict = vars(self)
+        return recursive_dump(prop_dict)
+
+def recursive_dump(value):
+    # recursively call dump() for nested objects to generate a json-serializable dict
+    if isinstance(value, dict):
+        return {key:recursive_dump(value[key]) for key in value}
+    elif isinstance(value, list):
+        return [recursive_dump(v) for v in value]
+    else:
+        try:
+            return value.dump()
+        except AttributeError:
+            return value
