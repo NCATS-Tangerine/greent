8e09d83ed3ab7308a99180d33e18ebac33ebcb55
cbizon <bizon@renci.org>
2018-09-28 08:06:05 -0400

    Crawl

--- crawler/crawl_all.py
+++ crawler/crawl_all.py
@@ -4,7 +4,7 @@ from greent import node_types
 from crawler.chemicals import load_chemicals
 from crawler.program_runner import load_all
 from crawler.disease_phenotype import load_diseases_and_phenotypes
-from crawler.omni import create_omnicache
+from crawler.omni import create_omnicache,update_omnicache
 from datetime import datetime as dt
 
 def poolrun(type1,type2,rosetta):
@@ -25,8 +25,10 @@ def crawl():
     #poolrun(node_types.DISEASE, node_types.GENE, rosetta)
     #poolrun(node_types.DISEASE, node_types.CHEMICAL_SUBSTANCE, rosetta)
     #poolrun(node_types.CHEMICAL_SUBSTANCE, node_types.DISEASE, rosetta)
-    poolrun(node_types.ANATOMICAL_ENTITY, node_types.PHENOTYPIC_FEATURE, rosetta)
+    #poolrun(node_types.GENE, node_types.CHEMICAL_SUBSTANCE, rosetta)
+    #poolrun(node_types.ANATOMICAL_ENTITY, node_types.PHENOTYPIC_FEATURE, rosetta)
     #create_omnicache(rosetta)
+    update_omnicache(rosetta,'CHEBI','CHEBI')
 
 #
 # Use this to clean up: should probably 
