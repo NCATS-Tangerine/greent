a12dd9af18cf211bb6af197a73b2df24e7647bd3
cbizon <bizon@renci.org>
2018-10-08 07:52:50 -0400

    disease

--- crawler/disease_phenotype.py
+++ crawler/disease_phenotype.py
@@ -2,14 +2,31 @@ from crawler.crawl_util import glom, dump_cache
 from builder.question import LabeledID
 from greent.util import Text
 
+def write_sets(sets,fname):
+    with open(fname,'w') as outf:
+        for s in sets:
+            outf.write(f'{s}\n')
+
+def write_dicts(dicts,fname):
+    with open(fname,'w') as outf:
+        for k in dicts:
+            outf.write(f'{k}\t{dicts[k]}\n')
+
+
 def load_diseases_and_phenotypes(rosetta):
     mondo_sets = build_exact_sets(rosetta.core.mondo)
+    write_sets(mondo_sets,'mondo_sets.txt')
     hpo_sets = build_sets(rosetta.core.hpo)
+    write_sets(hpo_sets,'hpo_sets.txt')
     meddra_umls = read_meddra()
+    write_sets(hpo_sets,'meddra_umls_sets.txt')
     dicts = {}
     glom(dicts,mondo_sets)
+    write_dicts(dicts,'mondo_dicts.txt')
     glom(dicts,hpo_sets)
+    write_dicts(dicts,'mondo_hpo_dicts.txt')
     glom(dicts,meddra_umls)
+    write_dicts(dicts,'mondo_hpo_meddra_dicts.txt')
     with open('disease.txt','w') as outf:
         dump_cache(dicts,rosetta,outf)
 
@@ -29,6 +46,13 @@ def build_exact_sets(o):
     return sets
 
 
+def norm(curie):
+    if Text.get_curie(curie) == 'MSH':
+        return f'MESH:{Text.un_curie(curie)}'
+    if Text.get_curie(curie) == 'SNOMEDCT_US':
+        return f'SNOMEDCT:{Text.un_curie(curie)}'
+    return curie
+
 def build_sets(o):
     sets = []
     mids = o.get_ids()
@@ -37,6 +61,7 @@ def build_sets(o):
         #just excise them here.  It's possible that we'll want to revisit this decision in the future.  If so,
         #then we probably will want to set a 'glommable' and 'not glommable' set.
         dbx = set([Text.upper_curie(x['id']) for x in o.get_xrefs(mid) if not x['id'].startswith('ICD')])
+        dbx = set([norm(x) for x in dbx])
         label = o.get_label(mid)
         mid = Text.upper_curie(mid)
         dbx.add(LabeledID(mid,label))
