0ffe14ee5e28082f3acf0f191d993256d54f3af9
Yaphet K <kebedey@renci.org>
2018-12-10 21:42:06 -0500

    Chmod change for task logs and an endpoint to retrive logs through api

--- builder/api/building.py
+++ builder/api/building.py
@@ -178,6 +178,34 @@ class TaskStatus(Resource):
 
 api.add_resource(TaskStatus, '/task/<task_id>')
 
+class TaskLog(Resource):
+    def get(self, task_id):
+        """
+        Get activity log for a task
+        ---
+        tags: [util]
+        parameters:
+          - in: path
+            name: task_id
+            description: ID of task
+            schema:
+                type: string
+            required: true
+        responses:
+            200:
+                description: text
+        """
+
+        task_log_file = os.path.join(os.environ['ROBOKOP_HOME'], 'task_logs', f'{task_id}.log')
+        if os.path.isfile(task_log_file):
+            with open(task_log_file, 'r') as log_file:
+                log_contents = log_file.read()
+            return log_contents, 200
+        else:
+            return 'Task ID not found', 404
+
+api.add_resource(TaskLog, '/task/<task_id>/log')
+
 class Operations(Resource):
     def get(self):
         """
