75d28b31329355dde481a6b7a4ddfebdb8253fad
YaphetKG <45075777+YaphetKG@users.noreply.github.com>
2019-05-07 09:25:31 -0400

    Readme updates (#11)
    * Updated scripts to accept backup docker compose file as a parmeter for more robustness
    
    * readme updates
    
    * some edits on setting up environment
    
    * some edits on setting up environment
    
    * added directories infront of command to make it more intuitive
    
    * shebang correction for reload script, some readme modification too
    
    * readme updates with docker build for the app
    
    * Neo4j ssl dir fix, for permission error on mounted dir
    
    * path correction for neo4j

--- deploy/robokopkg/scripts/backup.sh
+++ deploy/robokopkg/scripts/backup.sh
@@ -1,17 +1,56 @@
-#!/bin/sh
+#!/bin/bash
+
+compose_file="scripts/docker-compose-backup.yml"
+
+function printHelp(){
+    echo "
+        This script will take the back up of neo4j database. 
+        Usage:
+            ./backup.sh -c ./docker-compose-backup.yml 
+        Arguments:
+            -c   backup-compose-file       docker compose file.
+            
+            -h    help          display this message.
+
+    "
+}
+
+
+while getopts :hc: opt; do
+    case $opt in 
+        h) 
+        printHelp
+        exit
+        ;;
+        c) 
+        compose_file=$OPTARG
+        ;;
+        \?) 
+        echo "Invalid option -$OPTARG" 
+        printHelp
+        exit 1
+        ;;
+    esac
+done
 
 docker kill $(docker ps -f name=neo4j -q) 
 
-docker-compose -f scripts/docker-compose-backup.yml up -d
+docker-compose -f $compose_file up -d
 
 # ------------- back up process start 
 
 export backup_time=$(date +"%m%d%y-%I-%M-%S")
 
+# when killing containers New neo4j complains 'Active Logical log detected', 
+# and needs a clean shutdown :/
+
+docker exec $(docker ps -f name=neo4j -q) bash -c "bin/neo4j start; bin/neo4j stop"
+
+
 # 
 docker exec $(docker ps -f name=neo4j -q) bash bin/neo4j-admin dump --to data/graph.db.$backup_time.dump
 
-docker exec $(docker ps -f name=neo4j -q)  ln -s /data/graph.db.$backup_time.dump data/graph.db.latest.dump
+docker exec $(docker ps -f name=neo4j -q)  ln -sf /data/graph.db.$backup_time.dump data/graph.db.latest.dump
 
 # kill back-upper container
 docker kill $(docker ps -f name=neo4j -q)
