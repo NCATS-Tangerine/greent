879cd426aa138f8e4d6fe0ce631665de916242d9
Evan Morris <evandietzmorris@gmail.com>
2019-06-27 14:50:58 -0400

    added indexing to sqlite table, improved precaching for LD, refactoring

--- greent/services/ensembl.py
+++ greent/services/ensembl.py
@@ -3,7 +3,7 @@ from greent import node_types
 from greent.graph_components import KNode, LabeledID
 from greent.service import Service
 from greent.util import Text, LoggingUtil
-import logging,json,sqlite3,os,requests
+import logging,json,sqlite3,os,requests,pickle
 
 logger = LoggingUtil.init_logging(__name__, level=logging.DEBUG)#
 
@@ -36,7 +36,25 @@ class Ensembl(Service):
                                         </Dataset>
                                     </Query>"""
         self.gene_batch_url = 'http://www.ensembl.org/biomart/martservice'
-        self.gene_range_select_sql = "SELECT ensembl_id, start_pos, end_pos FROM genes WHERE (chromosome = ?) AND (? >= start_pos AND ? <= end_pos) OR (? >= start_pos AND ? <= end_pos) OR (? <= start_pos AND ? >= end_pos)"
+        
+        self.check_if_already_done_sql = "SELECT name FROM sqlite_master WHERE type='table' AND name='genes';"
+        
+        self.gene_entry_sql = "INSERT INTO genes (ensembl_id, gene_name, chromosome, start_pos, end_pos, gene_type, description) VALUES (?,?,?,?,?,?,?);"
+        
+        self.gene_range_select_sql = """SELECT ensembl_id, start_pos, end_pos 
+        FROM genes WHERE chromosome = ? AND ((? >= start_pos AND ? <= end_pos)
+        OR (? >= start_pos AND ? <= end_pos) OR (? <= start_pos AND ? >= end_pos));"""
+
+        self.genes_table_index_sql = "CREATE INDEX gene_composite on genes(chromosome, start_pos, end_pos, ensembl_id);"
+        self.genes_table_sql = """CREATE TABLE IF NOT EXISTS genes (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        ensembl_id text, 
+        gene_name text, 
+        chromosome INTEGER,
+        start_pos INTEGER, 
+        end_pos INTEGER, 
+        gene_type text,
+        description text);"""
 
     def create_or_connect_to_genes_db(self):
         
@@ -51,39 +69,25 @@ class Ensembl(Service):
         db_conn = sqlite3.connect(self.gene_db_path)
         db_cursor = db_conn.cursor()
 
-        check_if_already_done_sql = "SELECT name FROM sqlite_master WHERE type='table' AND name='genes'"
-        db_cursor.execute(check_if_already_done_sql)
+        db_cursor.execute(self.check_if_already_done_sql)
         if db_cursor.fetchone() is not None:
             # TODO we assume this means it's complete - it could have been created but not filled out
             # we could check the number of genes or something, but it will likely be changing
             self.gene_db_successfully_created = True
-            return
-
-        genes_table_sql = """
-        CREATE TABLE IF NOT EXISTS genes (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        ensembl_id text, 
-        gene_name text, 
-        chromosome INTEGER,
-        start_pos INTEGER, 
-        end_pos INTEGER, 
-        gene_type text,
-        description text)
-        """
-
-        db_cursor.execute(genes_table_sql)
-
-        gene_entry_sql = "INSERT INTO genes (ensembl_id, gene_name, chromosome, start_pos, end_pos, gene_type, description) VALUES (?,?,?,?,?,?,?)"
+            return True
 
         gene_counter = 0
         genes_response = requests.get(self.ensembl_genes_url)
         if genes_response.status_code == 200:
             genes_data = genes_response.text.splitlines()
-            # we assume the index order is predictable from url above - if we change that we need to change this
             if len(genes_data) > 1:
+                
+                db_cursor.execute(self.genes_table_sql)
+
                 for gene_line in genes_data[1:]:
                     gene_data = gene_line.split('\t')
                     try:
+                        # we assume the index order is predictable from url above - if we change that we need to change this
                         ensembl_id = gene_data[0]
                         gene_type = gene_data[1]
                         gene_name = gene_data[2]
@@ -98,22 +102,25 @@ class Ensembl(Service):
                         #elif chromosome == 'MT':
                         #    chromosome = 25
 
-                        db_cursor.execute(gene_entry_sql, (ensembl_id, gene_name, chromosome, start_pos, end_pos, gene_type, description))
+                        db_cursor.execute(self.gene_entry_sql, (ensembl_id, gene_name, chromosome, start_pos, end_pos, gene_type, description))
                         gene_counter += 1
 
                     except IndexError as e:
                         logger.error(f'Ensembl biomart genes call had an issue with one line: {e})')
+
+                db_cursor.execute(self.genes_table_index_sql)
+                db_conn.commit()
+                db_conn.close()
+                logger.info(f'Ensembl created a gene database with {gene_counter} entries!')
+                self.gene_db_successfully_created = True
+                return True
+
             else:
                 logger.error(f'Ensembl biomart genes call didnt find any matches! Thats not right!')
-                return
+                return False
         else:
             logger.error(f'Ensembl non-200 response from biomart genes call: {genes_response.status_code})')
-            return
-
-        db_conn.commit()
-        db_conn.close()
-        logger.info(f'Ensembl created a gene database with {gene_counter} entries!')
-        self.gene_db_successfully_created = True
+            return False
 
     def sequence_variant_to_gene(self, variant_node):
         
@@ -158,7 +165,9 @@ class Ensembl(Service):
         db_cursor.execute(self.gene_range_select_sql, (chromosome, flanking_min, flanking_min, flanking_max, flanking_max, flanking_min, flanking_max))
 
         genes_in_region = db_cursor.fetchall()
-        for gene_id, gene_start, gene_end in genes_in_region:
+        for gene_id_text, gene_start, gene_end in genes_in_region:
+            #cast this to make neo4j happy
+            gene_id = str(gene_id_text)
             #logger.info(f'Found matching gene: {gene_id},{gene_start},{gene_end}')
             gene_node = KNode(f'ENSEMBL:{gene_id}', name=f'{gene_id}', type=node_types.GENE)
             if start_position < gene_start:
@@ -182,31 +191,50 @@ class Ensembl(Service):
         population = '1000GENOMES:phase_3:MXL'
 
         return_results = []
-        dbsnp_curie_ids = variant_node.get_synonyms_by_prefix('DBSNP')
-        for dbsnp_curie in dbsnp_curie_ids:
-            variant_id = Text.un_curie(dbsnp_curie)
-            query_url = f'{self.url}{ld_url}{variant_id}/{population}{options_url}'
-            query_response = requests.get(query_url, headers={"Content-Type" : "application/json"})
-            if query_response.status_code == 200:
-                query_json = query_response.json()
-                variant_results = self.parse_ld_variants_from_ensembl(query_json)
-                for variant_info in variant_results:
-                    new_variant_id = variant_info[0]
-                    r_squared = variant_info[1]
-                    props = {'r2' : r_squared}
-                    new_variant_curie = f'DBSNP:{new_variant_id}'
-                    new_variant_node = KNode(new_variant_curie, name=f'{new_variant_id}', type=node_types.SEQUENCE_VARIANT)
-                    synonyms = self.cache.get(f'synonymize({new_variant_curie})') 
-                    if synonyms is None:
-                        synonyms = self.clingen.get_synonyms_by_other_ids(new_variant_node)
-                        self.cache.set(f'synonymize({new_variant_curie})', synonyms)
-                    for synonym in synonyms:
-                        if Text.get_curie(synonym.identifier) == 'CAID':
-                            caid_node = KNode(synonym.identifier, name=f'{new_variant_id}', type=node_types.SEQUENCE_VARIANT)
-                            edge = self.create_edge(variant_node, caid_node, 'ensembl.sequence_variant_to_sequence_variant', dbsnp_curie, self.var_to_var_predicate, url=query_url, properties=props)
-                            return_results.append((edge, caid_node))
-            else:
-                logger.error(f'Ensembl returned a non-200 response for {variant_node.identifier}: {query_response.status_code})')
+        with self.context.cache.redis.pipeline() as redis_pipe:
+            dbsnp_curie_ids = variant_node.get_synonyms_by_prefix('DBSNP')
+            for dbsnp_curie in dbsnp_curie_ids:
+                variant_id = Text.un_curie(dbsnp_curie)
+                query_url = f'{self.url}{ld_url}{variant_id}/{population}{options_url}'
+                query_response = requests.get(query_url, headers={"Content-Type" : "application/json"})
+                if query_response.status_code == 200:
+                    query_json = query_response.json()
+                    variant_results = self.parse_ld_variants_from_ensembl(query_json)
+                    for variant_info in variant_results:
+                        new_variant_id = variant_info[0]
+                        r_squared = variant_info[1]
+                        props = {'r2' : r_squared}
+                        new_variant_curie = f'DBSNP:{new_variant_id}'
+                        new_rsid_node = None
+                        synonyms = self.cache.get(f'synonymize({new_variant_curie})') 
+                        if synonyms is None:
+                            new_rsid_node = KNode(new_variant_curie, name=f'{new_variant_id}', type=node_types.SEQUENCE_VARIANT)
+                            synonyms = self.clingen.get_synonyms_by_other_ids(new_rsid_node)
+                            redis_pipe.set(f'synonymize({new_variant_curie})', pickle.dumps(synonyms))
+                        caid_count = 0
+                        caid_node = None
+                        for synonym in synonyms:
+                            if Text.get_curie(synonym.identifier) == 'CAID':
+                                caid_count += 1
+                                caid_node = KNode(synonym.identifier, name=f'{synonym.label}', type=node_types.SEQUENCE_VARIANT)
+                                edge = self.create_edge(variant_node, caid_node, 'ensembl.sequence_variant_to_sequence_variant', dbsnp_curie, self.var_to_var_predicate, url=query_url, properties=props)
+                                return_results.append((edge, caid_node))
+                                found_caid = True
+                        if caid_count == 1:
+                            # go ahead and precache this 
+                            redis_pipe.set(f'synonymize({caid_node.id})',  pickle.dumps(synonyms))
+                        elif caid_count == 0:
+                            if not new_rsid_node:
+                                new_rsid_node = KNode(new_variant_curie, name=f'{new_variant_id}', type=node_types.SEQUENCE_VARIANT)
+                            edge = self.create_edge(variant_node, new_rsid_node, 'ensembl.sequence_variant_to_sequence_variant', dbsnp_curie, self.var_to_var_predicate, url=query_url, properties=props)
+                            return_results.append((edge, new_rsid_node))
+
+                #elif query_response.status_code == 429:
+                #   handle the rate limiting by waiting and retrying
+                #
+                else:
+                    logger.error(f'Ensembl returned a non-200 response for {variant_node.id}: {query_response.status_code})')
+            redis_pipe.execute()
 
         return return_results
 
